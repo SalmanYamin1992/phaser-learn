ba72af587de87d81f6e72f06a86f2fe7a2a0f6af
SalmanYamin1992 <yaminsalman4@gmail.com>
2022-01-30 09:54:00 +0000

    added phaser ts def

--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
-
 import Phaser from "phaser";
 
-const config = {
+var config = {
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200 }
-    }
+    default: "arcade",
   },
   scene: {
-    preload: preload,
-    create: create
-  }
+    preload,
+    create,
+  },
 };
 
-new Phaser.Game(config);
+function preload() {
+  this.load.image("sky", "assets/sky.png");
+  this.load.image("bird", "assets/bird.png");
+}
+let bird;
+function create() {
+  // this.add.image(config.width / 2, config.height / 2, "sky");
+  this.add.image(0, 0, "sky").setOrigin(0, 0);
+  bird = this.physics.add
+    .sprite(config.width * 0.1, config.height / 2, "bird")
+    .setOrigin(0, 0);
 
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
+  bird.body.gravity.y = 200;
 }
 
-function create () {
-  this.add.image(400, 300, 'sky');
-}
+var game = new Phaser.Game(config);
